07834137a63fc555c047e4f59b1dce322e6ac348
china-zhangmy <china.zhangmy@hotmail.com>
2018-08-07 18:16:58 +0800

    Java-Core Thread

---
+++ src/thread/state/RunnableStateTest.java
@@ -0,0 +1,13 @@
+package thread.state;
+
+public class RunnableStateTest {
+
+    public static void main(String[] args) {
+        final ThreadTask task = new ThreadTask.Normal();
+
+        Thread thread = new Thread(() -> task.concreteTask(), "Thread 1");
+        thread.start();
+        System.out.println("线程[" + thread.getName() + "]的当前状态： " + thread.getState());
+    }
+
+}
---
+++ src/thread/thread_runnable_callable/ImplementsRunnable.java
@@ -0,0 +1,28 @@
+package thread.thread_runnable_callable;
+
+/**
+ * 优点：(1) 创建线程的同时可以继承其他的类，从而可以扩展类的功能。
+ * (2) 同一个实现Runnable接口的实例可以作为多个Thread的target,这样可以实现资源的共享。
+ * <p>
+ * 缺点：相对于继承Thread类，实现Runnable接口代码要繁琐一些
+ */
+public class ImplementsRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("[Thread Name] " + Thread.currentThread().getName() + " [Thread ID] " + Thread.currentThread().getId());
+    }
+
+    public static void main(String[] args) {
+        // 输出主线程的相关信息
+        System.out.println("[Thread Name] " + Thread.currentThread().getName() + " [Thread ID] " + Thread.currentThread().getId());
+
+        // 通过模板创建线程
+        ImplementsRunnable target = new ImplementsRunnable();
+        new Thread(target, "Thread 1").start();
+
+        // 通过内部类创建线程
+        new Thread(() -> System.out.println("[Thread Name] " + Thread.currentThread().getName() + " [Thread ID] " + Thread.currentThread().getId()), "Thread 2").start();
+    }
+
+}
